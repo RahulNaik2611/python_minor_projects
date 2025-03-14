8af3730b9927a98eed35403481e1a80028b6c34a
B Rahul Naik <banavathrahulnaik26@gmail.com>
2025-03-14 17:45:53 +0530

    Update Bill_tip_calculator.py

--- minor_project/Bill_tip_calculator.py
+++ minor_project/Bill_tip_calculator.py
@@ -1,1 +1,11 @@
+print("Welcome to the tip  calculator ")
 
+bill = float(input("What was the total bill ? 💸 INR" + "  "))
+
+tip = int(input("What percentage tip would you give 10,12 & 15" + " "))
+
+people = int(input("How many people want to split bill " + " "))
+
+bill_with_tip = (tip /100) * bill + bill
+
+print(bill_with_ti
