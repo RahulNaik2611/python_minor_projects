7d52f04fdb95834be4a7bd4445ee04a3094d695b
B Rahul Naik <banavathrahulnaik26@gmail.com>
2025-03-24 22:56:28 +0530

    Create Number_Guessing_Game.py

---
+++ minor_project/Number_Guessing_Game.py
@@ -0,0 +1,24 @@
+import  random
+
+
+
+number_To_guess = random.randint(1,100)
+
+while True:
+    try:
+        Guess = int(input("Enter the number here : "))
+
+        if Guess < number_To_guess:
+            print("Too low!")
+        elif Guess > number_To_guess:
+            print("Too high")
+        else:
+            print("Congratulations! you guessing the number.")
+
+
+
+    except ValueError:
+        print("Please Entre a valid number ")
+
+
+
